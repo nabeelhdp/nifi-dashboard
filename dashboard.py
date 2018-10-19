b241a7eb65665319b7c1f2b109d8f3cbe54c3253
nabeelhdp <32059729+nabeelhdp@users.noreply.github.com>
2018-10-19 22:19:28 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,6 @@
-
-from bottle import route, run, template
-from nifi_poll import get_flow_status,get_system_diagnostics,get_processor_stats,get_pg_details
+import json
+from bottle import route, run, template, static_file
+from nifipoll import get_flow_status,get_system_diagnostics,get_processor_stats,get_pg_details
 import socket
 
 
@@ -9,15 +9,29 @@ HOST = socket.getfqdn()
 pgstats = {}
 pgstats = get_pg_details()
 name = {}
-flow_files_queued = {}
+flowFilesQueued = {}
+bytesQueued = {}
+total_flowFilesQueued = 0
 
 for key,value in pgstats.iteritems():
 
   name[key] = value['name']
-  flow_files_queued[key] = value['flow_files_queued']
+  flowFilesQueued[key] = value['flowFilesQueued']
+  total_flowFilesQueued += value['flowFilesQueued']
+  bytesQueued[key] = value['bytesQueued']
 
-@route('/page1')
+print total_flowFilesQueued
+
+@route('/static/:path#.+#', name='static')
+def static(path):
+    return static_file(path, root='static')
+
+@route('/table')
 def serve_homepage():
-    return template('disp_table',pgnames = name, flowfilesqueued = flow_files_queued, cases = len(name))
+    return template('disp_table',pgnames = name, dataparam = bytesQueued, cases = len(name))
+
+@route('/pie')
+def serve_pie():
+    return template('pie', dataparam = json.dumps(flowFilesQueued), total = total_flowFilesQueued )
 
 run(host=HOST, port=8080, debug=True)
